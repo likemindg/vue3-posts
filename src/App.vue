6a6240cdc2e52fadf4632f6c239ad0940d7e7d67
leeyongtae~ <likemind@hanmail.net>
2023-03-05 22:18:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from '@/layouts/TheHeader.vue';
+import TheView from '@/layouts/TheView.vue';
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <TheVeiw></TheVeiw>
+</template>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+const app = createApp(App);
+
+app.mount('#app');
+
+import 'bootstrap/dist/js/bootstrap.js';
